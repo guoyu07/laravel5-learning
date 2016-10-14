d9ad3dfc505d1452b42ae9af24b1b5abcdd552b5
charles <forc2dm@gmail.com>
2016-10-14 16:39:49 +0800

    [D] [A] Using art ide-helper:models "App\Models\Todo" to generate doc hint.

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,6 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Task
+ *
+ * @property integer $id
+ * @property string $title
+ * @property string $description
+ * @property \Carbon\Carbon $created_at
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Task whereId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Task whereTitle($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Task whereDescription($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Task whereCreatedAt($value)
+ * @mixin \Eloquent
+ */
 class Task extends Model
 {
     //
--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -4,6 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Todo
+ *
+ * @property integer $id
+ * @property string $name
+ * @property string $tag
+ * @property string $description
+ * @property \Carbon\Carbon $created_at
+ * @property string $done_at
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Todo whereId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Todo whereName($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Todo whereTag($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Todo whereDescription($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Todo whereCreatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Todo whereDoneAt($value)
+ * @mixin \Eloquent
+ */
 class Todo extends Model
 {
     //
